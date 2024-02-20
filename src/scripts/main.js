311c351dcf09edc29b3a7d90c16914e861f09989
Kevin Oliveira <55069829+klooke@users.noreply.github.com>
2024-02-20 20:24:06 -0300

    feat: piano keys

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 const showKeysCheck = document.querySelector("#show-keys");
+const pianoKeys = document.querySelectorAll(".piano .key");
+
+const audioTune = new Audio();
+
+function playAudio(key) {
+  audioTune.src = `./res/audio/${key}.wav`;
+  audioTune.play();
+}
 
 showKeysCheck.onclick = () => {
   pianoKeys.forEach((key) => key.firstElementChild.classList.toggle("hide"));
 };
+
+pianoKeys.forEach((key) => {
+  key.onclick = () => playAudio(key.dataset.key);
+});
+
